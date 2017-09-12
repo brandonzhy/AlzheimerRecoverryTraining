665bdb14d50cbbddf61f17af2c2870f9146a89cf
brandonzhy <18814968108@163.com>
2017-09-13 04:05:36 +0800

    final-V1.8

--- app/src/main/java/com/stone/app/photoUpload/App.java
+++ app/src/main/java/com/stone/app/photoUpload/App.java
@@ -1,23 +1,23 @@
-package com.stone.app.photoUpload;
-
-import android.app.Application;
-
-import com.squareup.leakcanary.LeakCanary;
-
-/**
-
- */
-
-public class App extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        LeakCanary.install(this);
-    }
-}
+//package com.stone.app.photoUpload;
+//
+//import android.app.Application;
+//
+//import com.squareup.leakcanary.LeakCanary;
+//
+///**
+//
+// */
+//
+//public class App extends Application {
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+//        LeakCanary.install(this);
+//    }
+//}
